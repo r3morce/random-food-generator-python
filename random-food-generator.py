9901883dbf1eeca9e533e3ca3da18e1d4e9a3ca0
Mathias <r3morce@users.noreply.github.com>
2016-08-17 16:23:30 +0200

    update ranking in csv file after rolling

--- random-food-generator.py
+++ random-food-generator.py
@@ -14,11 +14,16 @@ class Ranking:
 min_ranking = 0
 max_ranking = 0
 rankings = []
+new_rows = []
+selected_food = ''
+csv_file_name = 'foods.csv'
 
-with open('foods.csv', 'r') as csvfile:
+with open(csv_file_name, 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=';', quotechar='"')
 
+    # create rankings
     for row in reader:
+        new_rows.append(row)
         food = row[0]
         ranking = row[1]
 
@@ -26,20 +31,34 @@ with open('foods.csv', 'r') as csvfile:
         rankings.append(Ranking(food, min_ranking, max_ranking))
         min_ranking += 100/float(ranking)
 
-csvfile.close()
+    pick = random.uniform(0.0, max_ranking)
 
-pick = random.uniform(0.0, max_ranking)
+    # show chances
+    print("Chances are:")
 
-print("Chances are:")
+    for ranking in rankings:
+        chance = ((ranking.max-ranking.min)/max_ranking)*100
+        print("\t{:2.2f}%\tfor {}".format(chance, ranking.food))
 
-for ranking in rankings:
-    chance = ((ranking.max-ranking.min)/max_ranking)*100
-    print("\t{:2.2f}%\tfor {}".format(chance, ranking.food))
+    raw_input("\nPress Enter to continue...")
 
-raw_input("\nPress Enter to continue...")
+    # roll random food
+    for ranking in rankings:
+        if pick >= ranking.min and pick <= ranking.max:
+            selected_food = ranking.food
+            time = time.strftime("%A %d.%m.%Y")
+            print("\n[{}] You have to eat {}\n".format(time, selected_food))
+            break
 
-for ranking in rankings:
-    if pick >= ranking.min and pick <= ranking.max:
-        time = time.strftime("%A %d.%m.%Y")
-        print("\n[{}] You have to eat {}\n".format(time, ranking.food))
-        break
+    # write
+    for new_row in new_rows:
+        if new_row[0] == selected_food:
+
+            new_ranking = int(new_row[1])+1
+            new_row[1] = new_ranking
+
+            writer_file = open(csv_file_name, 'w')
+            writer = csv.writer(writer_file, delimiter =';', quotechar='"')
+            writer.writerows(new_rows)
+
+            print("The odds for {} are decreased".format(selected_food))
